c4907b1e34743935d13f832e24fc15e9146d1e8a
Stephen Hutchings <stephen@creativelicence.com.au>
2019-09-17 14:55:30 +1000

    PMs in US

--- app/assets/archive/the-prime-ministers-in-america-temp/plugins/wpfront-scroll-top/js/wpfront-scroll-top.js
+++ app/assets/archive/the-prime-ministers-in-america-temp/plugins/wpfront-scroll-top/js/wpfront-scroll-top.js
@@ -1,51 +1,8 @@
-/*
- WPFront Scroll Top Plugin
- Copyright (C) 2013, WPFront.com
- Website: wpfront.com
- Contact: syam@wpfront.com
- 
- WPFront Scroll Top Plugin is distributed under the GNU General Public License, Version 3,
- June 2007. Copyright (C) 2007 Free Software Foundation, Inc., 51 Franklin
- St, Fifth Floor, Boston, MA 02110, USA
- 
- */
-
 (function () {
     window.wpfront_scroll_top = function (data) {
         var $ = jQuery;
-        
+
         var container = $("#wpfront-scroll-top-container").css("opacity", 0);
-
-        var css = {};
-        switch (data.location) {
-            case 1:
-                css["right"] = data.marginX + "px";
-                css["bottom"] = data.marginY + "px";
-                break;
-            case 2:
-                css["left"] = data.marginX + "px";
-                css["bottom"] = data.marginY + "px";
-                break;
-            case 3:
-                css["right"] = data.marginX + "px";
-                css["top"] = data.marginY + "px";
-                break;
-            case 4:
-                css["left"] = data.marginX + "px";
-                css["top"] = data.marginY + "px";
-                break;
-        }
-        container.css(css);
-
-        if (data.button_width == 0)
-            data.button_width = "auto";
-        else
-            data.button_width += "px";
-        if (data.button_height == 0)
-            data.button_height = "auto";
-        else
-            data.button_height += "px";
-        container.children("img").css({"width": data.button_width, "height": data.button_height});
 
         if (data.hide_iframe) {
             if ($(window).attr("self") !== $(window).attr("top"))
