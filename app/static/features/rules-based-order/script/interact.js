5fe2c4951468e17aed79c0491046a5ed658c4edc
brodysmith1 <62872303+brodysmith1@users.noreply.github.com>
2020-06-10 20:10:37 +1000

    Collapsable intro

--- app/static/features/rules-based-order/script/interact.js
+++ app/static/features/rules-based-order/script/interact.js
@@ -1,26 +1,8 @@
-var rtime;
-var timeout = false;
-var delta = 200;
+var vh = window.innerHeight;
 var headerVisible = false;
+var footerFixed = false;
+var collapsed = true;
 
-// Resize debounce
-$(window).resize(function() {
-  rtime = new Date();
-  if (timeout === false) {
-      timeout = true;
-      setTimeout(resizeend, delta);
-  }
-});
-// Resize callback
-function resizeend() {
-  if (new Date() - rtime < delta) {
-      setTimeout(resizeend, delta);
-  } else {
-      timeout = false;
-      setHistoryTimelines();
-      $('.x-scroller').each( (i, el) => showHideHistoryBtns(el) );
-  }
-}
 // x-scroll for history box
 function scrollHistory(btn, fw) {
   // fw indicates fw or back button
@@ -33,15 +15,17 @@ function togglePM(i) {
   let pms = ["rudd", "gillard", "abbott", "turnbull", "morrison"];
   let pm = pms[i];
   // Nav menu
+  $('.tiles').addClass('nav-tiles');
   $('.tiles').children('.active').removeClass('active');
   $('.tiles').children(`.tile-${i}`).addClass('active');
   // Timeline content
   $('.show').removeClass('show');
   $(`.card.wrap.${pm}`).addClass('show');
+
   scrollThis('html,body',`.card.wrap.${pm}`, -100)
 }
 function scrollThis(p, c, o) {
-  $(p).animate({scrollTop: $(c)[0].offsetTop + o}, 500);
+  $(p).animate({scrollTop: $(c)[0].offsetTop + o}, 400);
 }
 // Set historyTimeline horizontal widths
 function setHistoryTimelines() {
@@ -76,21 +60,95 @@ function toggleHeader(show) {
   let tiles = $('.tiles');
   if (show) {
     tiles.css('height', '100px');
-    tiles.addClass('header nav-tiles fixed');
+    tiles.addClass('header nav-tiles fixed-tl');
     headerVisible = true;
   }
   else {
-    tiles.css('height', '120px');
-    tiles.removeClass('header nav-tiles fixed');
+    tiles.css('height', 'initial');
+    tiles.removeClass('header nav-tiles fixed-tl');
     headerVisible = false;
   }
 }
 
-// Scroll event listener
-function headerCheck() {
-  let c = $(window).scrollTop();
-  let tiles = $('.tile-wrap')[0].offsetTop;
-  if (!headerVisible && c > tiles) {toggleHeader(true);}
-  else if (headerVisible && c < tiles) {toggleHeader(false);}
+// FIXED FOOTER
+function toggleFooter(show) {
+  if (show) {
+    $('.intro-footer').addClass('fixed-b');
+    footerFixed = true;
+  }
+  else {
+    $('.intro-footer').removeClass('fixed-b');
+    footerFixed = false;
+  }
 }
-window.addEventListener('scroll', headerCheck);
+
+function resetWrapHeight(p, c) {
+  let h = $(c).outerHeight(true);
+  $(p).css('min-height', h+'px');
+}
+function readMore() {
+  // TOGGLE VISIBLE TEXT
+  $('.intro-txt .collapse').toggleClass('hidden');
+  let pv = 48;
+  let th = $('.intro-txt').outerHeight(true) + $('.intro-wrap .title').outerHeight(true) + 2*pv;
+  let h = $('.intro-txt .hidden').length ? 0.9*vh : th;
+  $('.intro-wrap').animate({'max-height': h});
+  scrollThis('html,body', '.intro-wrap', 95);
+
+  // REMOVE REVEAL
+  $('.reveal').addClass('hidden');
+
+  // ADJUST FOOTER
+  $('.intro-footer')
+    .addClass('fixed-b txt-s')
+    .removeClass('txt-l')
+    .css('min-height', '3.5em');
+  setTimeout(() => {footerFixed = true;}, 500);
+  collapsed = false;
+}
+function snapScroll() {
+  let c = $(window).scrollTop();
+  $('section').forEach((s, i) => {
+      console.log(Math.abs(c - s.offsetTop));
+      Math.abs(c - s.offsetTop) < 100 ? scrollThis('html,body', 'section', 0) : "";
+  });
+}
+
+//onload event listener
+function onLoad() {
+  resetWrapHeight('.tile-wrap','.tiles');
+}
+
+// Resize event listener
+function resizeCallback() {
+  vh = window.innerHeight;
+  headerVisible ? "" : resetWrapHeight('.tile-wrap','.tiles');
+  // setHistoryTimelines();
+  // $('.x-scroller').each( (i, el) => showHideHistoryBtns(el) );
+}
+
+// onScroll event listener
+function scrollCheck() {
+  let c = $(window).scrollTop();
+
+  // toggle header
+  let y0 = $('.tile-wrap')[0].offsetTop;
+  let y1 = $('.tile.active').length ? $('.tile.active')[0].offsetTop : 99999;
+  if (!headerVisible && c > y1) {toggleHeader(true);}
+  else if (headerVisible && c < y0) {toggleHeader(false);}
+
+  // toggle footer
+  let fh = $('.intro-footer').outerHeight();
+  let ty = $('.tile-page')[0].offsetTop;
+  let y2 = ty - vh;
+  if (!collapsed) {
+    if (!footerFixed && c < y2 && c > fh) {toggleFooter(true);}
+    else if (footerFixed && c > y2 + fh || c < fh) {toggleFooter(false);}
+  }
+
+  // _.debounce(snapScroll, 0);
+}
+
+window.addEventListener('load', onLoad);
+window.addEventListener('scroll', scrollCheck);
+window.addEventListener('resize', _.debounce(resizeCallback, 200));
