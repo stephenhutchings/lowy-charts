b0489b47da875b7982e7df1e9690f21c15c68d4f
brodysmith1 <62872303+brodysmith1@users.noreply.github.com>
2020-08-05 13:04:21 +1000

    Experimental fix for cite:hover in Safari

--- app/static/features/rules-based-order/script/annotation.js
+++ app/static/features/rules-based-order/script/annotation.js
@@ -111,6 +111,8 @@ function createAnnotations() {
     el.appendChild(document.createElement("span")).innerHTML = data;
 
     isComment ? el.classList.add('bg-ccnr') : el.innerHTML += '^';
+
+    el.addEventListener('click', void(0));
   });
 
 }
