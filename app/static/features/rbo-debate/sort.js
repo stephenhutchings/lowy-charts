7b51680cbfab9de16904c723ef56d78ded1d99dd
brodysmith1 <62872303+brodysmith1@users.noreply.github.com>
2020-08-17 16:39:03 +1000

    Perfected sort positioning

--- app/static/features/rbo-debate/sort.js
+++ app/static/features/rbo-debate/sort.js
@@ -35,12 +35,14 @@ function initAnimateSort() {
 
 function focus(el) {
 
-  let i, targets;
+  let i, targets, heights, hSum, tBlock, ti;
   let t = el.offsetTop;
   let h = el.offsetHeight;
   let map = JSON.parse(el.dataset.map);
+  let lhs = map[0] < 8 ? true : false;
   let faders = document.querySelectorAll('.list-item, .list > h3, .list > p');  // Elements targeted for fading
   let sorters = document.querySelectorAll('.list-item');                        // Elements targeted for sorting
+  let bracket = document.querySelector('.bracket');                             // Bracket to wrap around sorted els
 
   el.querySelector('.read-more').classList.remove('no-ptr');  // Activate link on 'read article' button
   el.querySelector('.read-more').classList.add('show');       // Show 'read article' button
@@ -49,11 +51,32 @@ function focus(el) {
 
   faders.forEach( (item, i) => item === el ? "" : item.classList.add('fade') );
 
-  for (i=0; i < map.length; i++ ) {
-    targets = sorters[map[i]-1];
-    targets.classList.remove('fade');
-    targets.style.top = t + (1 + 2*i - map.length)*(h/2) + "px";
+  targets = map.map( (v,i) => sorters[v-1] );       // Get DOM elements
+  heights = targets.map( (el) => el.offsetHeight ); // Get their heights
+  hSum = heights.reduce( (sum, h) => sum + h );     // Sum all their heights
+
+  if (targets.length > 1) {                         // Set top of mapped elements
+    tBlock = t + h/2 - hSum/2;
+    tBlock < 0 ? tBlock = 0 : "";
+    targets.forEach( (el, i) => {
+      el.classList.remove('fade');
+      ti = heights.reduce( (sum, h, j) => j<=i ? sum + h : sum ); // Cumulative height of items thus far
+      ti -= heights[i];                                           // Minus height of this item
+      el.style.top = tBlock + ti + "px";                          // Offset this from the block top
+    });
   }
+  else {
+    tBlock = t;
+    targets[0].classList.remove('fade');
+    targets[0].style.top = tBlock + "px";
+  }
+
+
+  bracket.style.top = tBlock + "px";
+  bracket.style.height = hSum-10 + "px";
+  bracket.classList.remove('hide');
+  lhs ? bracket.classList.remove('bracket-left') : bracket.classList.remove('bracket-right');
+  lhs ? bracket.classList.add('bracket-right') : bracket.classList.add('bracket-left');
 
   isFocused = true;
 }
@@ -64,11 +87,14 @@ function unfocus (el) {
   let sortElArr = [...document.querySelectorAll('.list-item')];                // Object list of sorting elements
   let focusedHeading = list.querySelector('h2 a:not(.no-ptr)');
   let focusedButton = list.querySelector('.read-more:not(.no-ptr)');
+  let bracket = document.querySelector('.bracket');                             // Bracket to wrap around sorted els
 
   focusedHeading.classList.remove('txt-red');
   focusedHeading.classList.add('no-ptr');
   focusedButton.classList.remove('show');
   focusedButton.classList.add('no-ptr');
+
+  bracket.classList.add('hide');
 
   faders.forEach( el => el.classList.remove('fade') );
 
@@ -84,8 +110,8 @@ function spreadY(a) {
 
   let i = y = 0;
   let n = a.length/2;
-  let itemMargin = 20;
-  let listMargin = 70
+  let itemMargin = 0;
+  let listPaddingBottom = 70;
   let list = document.querySelector('.list');
 
   for (i; i < n; i++) {
@@ -93,6 +119,6 @@ function spreadY(a) {
     y += Math.max(a[i].offsetHeight, a[i+n].offsetHeight) + itemMargin;
   }
 
-  list.style.height = document.body.scrollHeight - list.offsetTop + listMargin + "px";
+  list.style.height = document.body.scrollHeight - list.offsetTop + listPaddingBottom + "px";
 
 }
