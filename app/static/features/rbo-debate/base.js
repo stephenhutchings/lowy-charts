0098d4eb4845777eb5755e554d09c44e77fa7a6f
Brody Smith <62872303+brodysmith1@users.noreply.github.com>
2020-09-02 08:02:12 +1000

    Read more on front page

--- app/static/features/rbo-debate/base.js
+++ app/static/features/rbo-debate/base.js
@@ -1,3 +1,10 @@
 function readmore() {
-  document.querySelector('#read-more').classList.toggle('no-max-h');
+  const reveal = document.querySelector('#read-more');
+  const btns = document.querySelectorAll('.readmore-label');
+
+  reveal.classList.toggle('lg-max-h');
+  btns.forEach( b => {
+    b.classList.toggle('hide');
+    b.classList.toggle('no-ptr-ev');
+  });
 }
