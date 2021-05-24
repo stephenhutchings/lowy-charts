b0dcf09a00ead8f4c2f0f61945608288c6242e85
Brody Smith <62872303+brodysmith1@users.noreply.github.com>
2021-05-24 21:41:18 +1000

    Map Interactivity

--- app/static/charts/scs-challenge/map/script.js
+++ app/static/charts/scs-challenge/map/script.js
@@ -1,6 +1,6 @@
-const ASPECT_RATIO = 895.5 / 954
+const ASPECT_RATIO = 954 / 895.5
 
-var vw
+var vh
 var map = document.querySelector('#map')
 
 const china = document.querySelectorAll('#China, [data-name=china]')
@@ -10,8 +10,23 @@ const malaysia = document.querySelectorAll('#Malaysia, [data-name=malaysia]')
 const indonesia = document.querySelectorAll('#Indonesia, [data-name=indonesia]')
 const philippines = document.querySelectorAll('#Philippines, [data-name=philippines]')
 
-const maintainAspectRatio = () => map.style.height = ASPECT_RATIO * vw  + 'px'
-const update = () => vw = map.getBoundingClientRect().width
+const maintainAspectRatio = h => map.style.width = ASPECT_RATIO * h + 'px'
 
-window.onload = maintainAspectRatio
-window.addEventListener("resize", () => { update(); maintainAspectRatio(); console.log('basd'); })
+const update = () => {
+  vh = window.innerHeight
+  let bb = map.getBoundingClientRect()
+  let overflow = vh - bb.top + bb.height
+  if (overflow > 0) {
+    maintainAspectRatio(vh - bb.top)
+  } else if (overflow < 0) {
+    map.removeAttribute('style')
+  }
+}
+
+const toggle = (country, btn) => {
+  btn.classList.toggle('active')
+  country.forEach(c => c.classList.toggle('opacity-0'))
+}
+
+window.onload = update
+window.addEventListener("resize", update)
