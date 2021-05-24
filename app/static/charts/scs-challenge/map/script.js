1acd4a0bfc912be74da03c64f3b5db7b416df5f8
Brody Smith <62872303+brodysmith1@users.noreply.github.com>
2021-05-24 18:03:51 +1000

    SCS map toggling

--- app/static/charts/scs-challenge/map/script.js
+++ app/static/charts/scs-challenge/map/script.js
@@ -3,19 +3,15 @@ const ASPECT_RATIO = 895.5 / 954
 var vw
 var map = document.querySelector('#map')
 
-const china = document.querySelector('#China')
-const brunei = document.querySelector('#Brunei')
-const vietnam = document.querySelector('#Vietnam')
-const malaysia = document.querySelector('#Malaysia')
-const indonesia = document.querySelector('#Indonesia')
-const philippines = document.querySelector('#Philippines')
+const china = document.querySelectorAll('#China, [data-name=china]')
+const brunei = document.querySelectorAll('#Brunei, [data-name=brunei]')
+const vietnam = document.querySelectorAll('#Vietnam, [data-name=vietnam]')
+const malaysia = document.querySelectorAll('#Malaysia, [data-name=malaysia]')
+const indonesia = document.querySelectorAll('#Indonesia, [data-name=indonesia]')
+const philippines = document.querySelectorAll('#Philippines, [data-name=philippines]')
 
 const maintainAspectRatio = () => map.style.height = ASPECT_RATIO * vw  + 'px'
 const update = () => vw = map.getBoundingClientRect().width
 
 window.onload = maintainAspectRatio
 window.addEventListener("resize", () => { update(); maintainAspectRatio(); console.log('basd'); })
-
-indonesia.addEventListener("click", () => {
-  console.log('indo')
-})
